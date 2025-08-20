d9c3ff2c4055a8db8a2676a0a07cc6547c6c206f
R Vetri Vignesh <rvetrivignesh@Rs-MacBook-Air.local>
2025-08-20 19:18:56 +0530

    add a simple footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SiteFooter: React.FC = () => {
+  return (
+    <footer className="py-6 text-center text-[var(--secondary-text)]">
+      <p>&copy; 2025 EasyCode Portal. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default SiteFooter;
