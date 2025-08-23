34cafc2a1f4e190a49569103d96c5ffe61499015
R Vetri Vignesh <rvetrivignesh@Rs-MacBook-Air.local>
2025-08-23 08:27:48 +0530

    add welcome page to app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import './App.css'
 import Header from './components/Header';
-import WelcomePage from './pages/WelcomePage';
 
 function App() {
   return (
     <>
       <Header />
-      <WelcomePage />
     </>
   )
 }
