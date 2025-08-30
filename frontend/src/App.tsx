b6561d2de8cf50b8801091eb3f544057da4d06cd
R Vetri Vignesh <rvetrivignesh@Rs-MacBook-Air.local>
2025-08-30 23:15:39 +0530

    add protected routes for admin login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,27 @@
-import './App.css'
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import WelcomePage from './pages/WelcomePage';
+import AdminDashboard from './pages/AdminDashboard';
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   return (
-    <>
+    <Router>
       <Header />
-      <WelcomePage />
-    </>
-  )
+      <Routes>
+        <Route path="/" element={<WelcomePage />} />
+        <Route
+          path="/admin"
+          element={
+            <ProtectedRoute>
+              <AdminDashboard />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
