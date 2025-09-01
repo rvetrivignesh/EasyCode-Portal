2da64574e65d6cbe7552fb997ce26e46ea419906
R Vetri Vignesh <rvetrivignesh@Rs-MacBook-Air.local>
2025-09-01 14:12:43 +0530

    add route for student dashboard

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import WelcomePage from './pages/WelcomePage';
 import AdminDashboard from './pages/Admin/AdminDashboard';
+import StudentDashboard from './pages/Student/StudentDashboard';
 import ProtectedRoute from './components/Admin-Components/ProtectedRoute';
 
 function App() {
@@ -19,6 +20,7 @@ function App() {
             </ProtectedRoute>
           }
         />
+        <Route path="/student" element={<StudentDashboard />} />
       </Routes>
     </Router>
   );
